ba02d429b7fab1c1215374f856f8dbeab87c2080
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-03-21 10:29:49 +0100

    changed host on config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,18 @@
 module.exports = {
-  apps: [
-    {
-      name: "open-recipes",
-      script: "npm",
-      args: "start",
-    },
-  ],
+  apps: [{
+    name: "open-recipes",
+    script: "npm",
+    args: "start",
+  }, ],
   deploy: {
     production: {
       user: "ubuntu",
-      host: "ec2-18-218-100-30.us-east-2.compute.amazonaws.com",
+      host: "3.139.143.120",
       key: "~/.ssh/open_recipes.pem",
       ref: "origin/main",
       repo: "git@github.com:AndreaUndecimo/opensource-recipes-server.git",
       path: "/home/ubuntu/opensource-recipes-server",
-      "post-deploy":
-        "npm install && pm2 startOrRestart ecosystem.config.js",
+      "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js",
     },
   },
 };
